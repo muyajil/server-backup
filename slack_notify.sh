0060b3c00cf0ca85e6c1ce29e88b37d4c4b1405f
Mohammed <mohammed@ajil.ch>
2021-01-21 07:30:40 +0000

    move gsuite backup to better location

--- slack_notify.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ "$DUPLICATI__PARSED_RESULT" != "Success" ]
-then
-    curl -s -X POST -H 'Content-type: application/json' --data '{"text":"There was a problem with the '$DUPLICATI__backup_name' backup!"}' ${SLACK_WEBHOOK}
-fi
